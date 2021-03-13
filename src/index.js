a983934ae4325a4078cee8c3a8d43adc669e2864
Stanislau <pontty232@gmail.com>
2021-03-13 13:13:34 +0300

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const bracesObj = {};
+
+  for(pair of bracketsConfig) {
+      bracesObj[pair[1]] = pair[0];
+  };
+
+  const bracesClose = Object.keys(bracesObj);
+
+  return str.split('').reduce((stack, brace) => {
+    bracesClose.includes(brace) && stack[stack.length - 1] === bracesObj[brace] ? stack.pop() : stack.push(brace);
+    return stack;
+  }, []).length === 0;
+
+};
